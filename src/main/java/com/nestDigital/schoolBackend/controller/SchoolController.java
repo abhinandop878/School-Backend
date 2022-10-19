f4e814b091e0cc82c74728feb6100b9f9bc6a078
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-20 02:24:58 +0530

    School backend

---
+++ src/main/java/com/nestDigital/schoolBackend/controller/SchoolController.java
@@ -0,0 +1,7 @@
+package com.nestDigital.schoolBackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SchoolController {
+}
---
+++ src/test/java/com/nestDigital/schoolBackend/SchoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestDigital.schoolBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
